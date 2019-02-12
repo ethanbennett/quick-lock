da7385eb992fd14a528a17c25ddc54460a57ba68
ethanbennett <ebennett19@gmail.com>
2019-02-11 22:00:58 -0700

    Rename

--- src/QuickWipe.sol
+++ src/QuickWipe.sol
@@ -8,7 +8,7 @@ contract SaiProxyInterface {
     function wipe(address, bytes32, uint, address) public payable;
 }
 
-contract QuickLock {
+contract QuickWipe {
     function buyAndWipe(address oasisDirectProxy, address saiProxy, address otc, address dai, address weth, address tub, uint buyAmount, bytes32 cup) public payable {
         if (msg.value > 0) {
             OasisDirectInterface(oasisDirectProxy).buyAllAmountPayEth.value(msg.value)(otc, dai, buyAmount, weth);
